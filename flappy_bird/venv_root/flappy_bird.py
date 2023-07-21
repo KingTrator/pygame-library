83283bc87ab8d178ba14e834e39c7568b183c009
KingTrator <georgehenriquessantos@gmail.com>
2023-07-21 01:33:13 -0300

    ajuste fino

--- flappy_bird/venv_root/flappy_bird.py
+++ flappy_bird/venv_root/flappy_bird.py
@@ -4,7 +4,7 @@ import random
 
 TELA_LARGURA = 500
 TELA_ALTURA = 800
-os.chdir(r'C:\Users\Win10\OneDrive\Documentos\Estudos\python-libraries\pygame-library\flappy_bird')
+os.chdir(r'C:\caminhoCorreto\flappy_bird')
 IMAGEM_CANO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'pipe.png')))
 IMAGEM_CHAO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'base.png')))
 IMAGEM_BACKGROUND = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bg.png')))
